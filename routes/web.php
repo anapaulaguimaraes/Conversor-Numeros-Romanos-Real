012c60600b3a9ad4d4c9e60bd3a5db5fda3fdbba
anapaulaguimaraes <anaapgg@gmail.com>
2024-08-04 15:41:55 -0300

    :sparkles: Criado rotas para o converte real e romano

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConversorController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('conversor');
 });
+
+Route::post('/converte-romano', [ConversorController::class, 'converteRomano']);
+Route::post('/converte-real', [ConversorController::class, 'converteReal']);
